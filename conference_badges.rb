a0ea4054441333b2972492f5303a354a59d45a82
Chris Franklin <cf73021@gmail.com>
2018-07-19 13:36:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  names.each do |name|
+    name = badge_maker(name)
+  end
+  return names
+end
